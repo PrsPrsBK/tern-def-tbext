6e2b8ec54d7dd6d2336488fcc9497d0355a77667
PrsPrsBK <prsprsbk@gmail.com>
2019-02-12 19:48:05 +0900

    feat: daily-check both mozilla-xxx and comm-xx

--- daily-check.ps1
+++ daily-check.ps1
@@ -1,32 +1,33 @@
 
 Param(
-  [Parameter(Position = 0, Mandatory = $true)]
+  [Parameter(Mandatory = $true)]
+  [ValidateScript({
+    if(-Not (Test-Path $_) -Or (Resolve-Path $_).Provider.Name -ne "FileSystem") {
+      throw "Please specify valid mozilla-xxx repository path : '$_'"
+    }
+    return $true
+  })]
+  [string]$MozillaRepo,
+  [Parameter(Mandatory = $true)]
   [ValidateScript({
     if(-Not (Test-Path $_) -Or (Resolve-Path $_).Provider.Name -ne "FileSystem") {
       throw "Please specify valid comm repository path : '$_'"
     }
     return $true
   })]
-  [string]$comm_repo,
-  [switch]$SkipFetch
+  [string]$CommRepo,
+  [switch]$SkipUpdate
 )
 
 Begin {
-  Get-Command hg -ErrorAction:Ignore | Out-Null
-  if($? -eq $false) {
-    throw "hg does not exist on the PATH."
-  }
-  if($SkipFetch) {
-    Write-Host "Skip 1st phase: no fetch from remote."
-  }
-  else {
-    $hg_proc = (Start-Process -FilePath "hg" -ArgumentList "incoming -R $comm_repo --quiet" -NoNewWindow -Wait -PassThru)
+  Function Update-Repository($repo_path) {
+    $hg_proc = (Start-Process -FilePath "hg" -ArgumentList "incoming -R $repo_path --quiet" -NoNewWindow -Wait -PassThru)
     Wait-Process -Id $hg_proc.id
     # System.Diagnostics.Process.ExitCode with -Wait is necessary. $LASTEXITCODE does not work.
     if($hg_proc.ExitCode -eq 0) {
       [ValidateSet("y","n")]$yn = Read-Host "incomings may exist. hg pull -u? [y/n]"
       if($yn -eq "y") {
-        Start-Process -FilePath "hg" -ArgumentList "pull -u -R $comm_repo" -NoNewWindow -PassThru | Wait-Process
+        Start-Process -FilePath "hg" -ArgumentList "pull -u -R $repo_path" -NoNewWindow -PassThru | Wait-Process
       }
       else {
         Write-Host "not 'yes', so nothing pulled. "
@@ -37,6 +38,21 @@ Begin {
     }
     $hg_proc = $null
   }
+
+  Get-Command hg -ErrorAction:Ignore | Out-Null
+  if($? -eq $false) {
+    throw "hg does not exist on the PATH."
+  }
+  if($SkipUpdate) {
+    Write-Host "Skip 1st phase: no fetch from remote."
+  }
+  else {
+    Write-Host "1st phase: update. fetch from remote."
+    Write-Host "Fetch: from Mozilla Repository"
+    Update-Repository $MozillaRepo
+    Write-Host "Fetch: from Comm Repository"
+    Update-Repository $CommRepo
+  }
   Write-Host "Next phase: check if we need to update or not."
 }
 
@@ -45,9 +61,23 @@ Process {
   $cset_pubed =  (Join-Path -Path $script_path -ChildPath "cset_pubed.log")
   $cset_today =  (Join-Path -Path $script_path -ChildPath "cset.log")
 
-  Start-Job -ArgumentList $comm_repo, $cset_today -ScriptBlock {
+  New-Item $cset_today -ItemType File -Force | Out-Null
+  Start-Job -ArgumentList $MozillaRepo, $cset_today -ScriptBlock {
     Param($repo, $log)
-    hg log -l 3 -R $repo -I (Join-Path -Path $repo -ChildPath "/mail/components/extensions/schemas/*.json") --removed --template status > $log
+    hg log -l 3 -R $repo -I (Join-Path -Path $repo -ChildPath "/toolkit/components/extensions/schemas/*.json") -X (Join-Path -Path $repo -ChildPath "/toolkit/components/extensions/schemas/manifest.json") --removed --template status `
+    | Add-Content $log
+  } | Wait-Job | Receive-Job | Remove-Job
+
+  Start-Job -ArgumentList $MozillaRepo, $cset_today -ScriptBlock {
+    Param($repo, $log)
+    hg log -l 3 -R $repo -I (Join-Path -Path $repo -ChildPath "/browser/components/extensions/schemas/*.json") --removed --template status `
+    | Add-Content $log
+  } | Wait-Job | Receive-Job | Remove-Job
+
+  Start-Job -ArgumentList $CommRepo, $cset_today -ScriptBlock {
+    Param($repo, $log)
+    hg log -l 3 -R $repo -I (Join-Path -Path $repo -ChildPath "/mail/components/extensions/schemas/*.json") --removed --template status `
+    | Add-Content $log
   } | Wait-Job | Receive-Job | Remove-Job
 
   if(Test-Path -Path $cset_pubed) {
